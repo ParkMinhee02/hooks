96a629e783b99f6db77d69a936f089719462bc34
ParkMinhee02 <sundu32672@naver.com>
2024-10-04 19:47:24 +0900

    hook 추가

---
+++ usememo/src/App1.js
@@ -0,0 +1,42 @@
+import { useMemo, useState } from 'react';
+import './App.css';
+
+const hardCalculate = (number) => {
+	console.log('어려운 계산');
+	for(let i = 0; i< 9999999; i++) {}
+	return number + 10000;
+};
+const easyCalculate = (number) => {
+	console.log('쉬운 계산기');
+	return number+1
+}
+
+function App() {
+	/* const value = useMemo(()=> {
+		return 내용();
+	},[]); */
+
+	const [hardNumber, setHardNumber] = useState(1);
+	const [easyNumber, setEasyNumber] = useState(1);
+	const hardSum = useMemo(() => {
+		return hardCalculate(hardNumber)
+	},[hardNumber]);
+	const easySum = easyCalculate(easyNumber);
+
+  return (
+    <div className="App">
+		<div>
+			<h3>어려운 계산기</h3>
+			<input type="number" value={hardNumber} onChange={(e) => setHardNumber(parseInt(e.target.value))} />
+			<span> + 10000 = {hardSum}</span>
+		</div>
+		<div>
+			<h3>쉬운 계산기</h3>
+			<input type="number" value={easyNumber} onChange={(e) => setEasyNumber(parseInt(e.target.value))} />
+			<span> + 1 = {easySum}</span>
+		</div>
+    </div>
+  );
+}
+
+export default App;
